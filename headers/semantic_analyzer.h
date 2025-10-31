51262da6049041abb1d9e9d781baf2a20cd47974
Samjoash9 <samjoashantonio@gmail.com>
2025-10-31 20:03:58 +0800

    done semantic analysis

--- headers/semantic_analyzer.h
+++ headers/semantic_analyzer.h
@@ -6,7 +6,6 @@
 
 #define SEM_MAX_TEMPS 2048
 
-/* semantic type (currently int/char/unknown; arithmetic treated as int) */
 typedef enum
 {
     SEM_TYPE_UNKNOWN = 0,
@@ -14,28 +13,26 @@ typedef enum
     SEM_TYPE_CHAR
 } SEM_TYPE;
 
-/* Temp container used by semantic analysis only */
 typedef struct
 {
-    int id; /* unique id */
+    int id;
     SEM_TYPE type;
-    int is_constant; /* 1 if compile-time-known */
-    long int_value;  /* valid iff is_constant and numeric */
-    ASTNode *node;   /* pointer to AST node (for diagnostics) */
+    int is_constant;
+    long int_value;
+    ASTNode *node;
 } SEM_TEMP;
 
-/* known compile-time variable value (semantic-only store) */
 typedef struct KnownVar
 {
     char *name;
     SEM_TEMP temp;
-    int initialized; /* 1 if we know it was initialized (semantic-known) */
-    int used;        /* 1 if variable was used somewhere (semantic pass) */
+    int initialized;
+    int used;
     struct KnownVar *next;
 } KnownVar;
 
 /* Public API */
-int semantic_analyzer(void);    /* run semantic analysis on global syntax_tree */
-int semantic_error_count(void); /* return number of semantic errors found */
+int semantic_analyzer(void);
+int semantic_error_count(void);
 
 #endif
